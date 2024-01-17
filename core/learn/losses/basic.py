b9f7cae3b9c8897d71dd8c51fff53965b0cd72e2
carefree0910 <syameimaru.saki@gmail.com>
2024-01-17 10:52:15 +0800

    ✨Implemented `CrossEntropyLoss`

--- core/learn/losses/basic.py
+++ core/learn/losses/basic.py
@@ -2,11 +2,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch import Tensor
+from typing import Tuple
 from typing import Optional
 
 from .schema import register_loss
 from ..schema import ILoss
 from ..schema import TrainerState
+from ..constants import INPUT_KEY
 from ..constants import LABEL_KEY
 from ..constants import PREDICTIONS_KEY
 from ...toolkit.array import corr
@@ -56,8 +58,34 @@ class CorrelationLoss(ILoss):
         return -corr(predictions, labels, get_diagonal=True).mean()
 
 
+def get_stats(predictions: Tensor, labels: Tensor) -> Tuple[Tensor, Tensor]:
+    log_prob_mat = F.log_softmax(predictions, dim=1)
+    nll_losses = -log_prob_mat.gather(dim=1, index=labels)
+    return log_prob_mat, nll_losses
+
+
+@register_loss("cross_entropy")
+class CrossEntropyLoss(ILoss):
+    def __init__(self, *, is_auto_regression: bool = False):
+        super().__init__()
+        self.is_auto_regression = is_auto_regression
+
+    def forward(
+        self,
+        forward_results: tensor_dict_type,  # type: ignore
+        batch: tensor_dict_type,
+        state: Optional[TrainerState] = None,
+    ) -> Tensor:
+        label_key = INPUT_KEY if self.is_auto_regression else LABEL_KEY
+        predictions = forward_results[PREDICTIONS_KEY]
+        labels = batch[label_key]
+        _, nll_losses = get_stats(predictions, labels)
+        return nll_losses.mean()
+
+
 __all__ = [
     "BCELoss",
     "MSELoss",
     "CorrelationLoss",
+    "CrossEntropyLoss",
 ]
