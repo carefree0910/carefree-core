493d1025a484a737a7d47f4263c00e9d48ed32b4
carefree0910 <syameimaru.saki@gmail.com>
2024-01-23 16:48:32 +0800

    ✅⚡️Optimized `test_titanic`

--- tests/test_learn/test_titanic.py
+++ tests/test_learn/test_titanic.py
@@ -3,14 +3,15 @@ import subprocess
 
 from pathlib import Path
 
+from ddp_titanic_task import main
+
 
 ddp_task_path = str(Path(__file__).parent / "ddp_titanic_task.py")
 
 
 class TestTitanic(unittest.TestCase):
     def test_titanic(self) -> None:
-        cmd = ["python", ddp_task_path]
-        subprocess.run(cmd, check=True)
+        main()
 
     def test_titanic_ddp(self) -> None:
         cmd = ["accelerate", "launch", "--num_processes=2", ddp_task_path]
