05a6d25092ab8ff506f2c4caca5cd292665ac80f
carefree0910 <syameimaru.saki@gmail.com>
2024-02-29 23:51:22 +0800

    ✅Enhanced `test_wandb_callback`

--- tests/test_learn/test_callbacks.py
+++ tests/test_learn/test_callbacks.py
@@ -5,17 +5,17 @@ import core.learn as cflearn
 
 class TestCallbacks(unittest.TestCase):
     def test_wandb_callback(self) -> None:
-        data, in_dim, out_dim, _ = cflearn.testing.linear_data()
+        data, in_dim, out_dim, _ = cflearn.testing.linear_data(use_validation=True)
         config = cflearn.Config(
             module_name="linear",
             module_config=dict(input_dim=in_dim, output_dim=out_dim, bias=False),
             scheduler_name="warmup",
             loss_name="mse",
-            num_steps=10**4,
             callback_names="wandb",
             callback_configs=dict(wandb=dict(anonymous="must")),
+            num_steps=20,
+            log_steps=4,
         )
-        config.to_debug().num_steps = 10  # comment this line to disable debug mode
         cflearn.TrainingPipeline.init(config).fit(data)
 
     def test_nan_detector_callback(self) -> None:
