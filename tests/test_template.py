a0622c98e72a2a0fe9232ce140841c39f01a0310
carefree0910 <syameimaru.saki@gmail.com>
2024-07-01 11:24:38 +0800

    ✅Updated `test_template`

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,9 +1,12 @@
 import unittest
 
+from core.parameters import OPT
+
 
 class TestTemplate(unittest.TestCase):
     def test_template(self) -> None:
         self.assertEqual(1 + 1, 2)
+        self.assertEqual(OPT.env_key, "CFCORE_ENV")
 
 
 if __name__ == "__main__":
