f2eaadae11373de1dadcd12627b72dfd29c82913
carefree0910 <syameimaru.saki@gmail.com>
2024-07-02 16:22:36 +0800

    🐛Fixed `BuildCallbacksBlock`

--- core/learn/pipeline/blocks/basic.py
+++ core/learn/pipeline/blocks/basic.py
@@ -358,6 +358,7 @@ class BuildCallbacksBlock(Block):
             self.callbacks = [
                 LogMetricsMsgCallback(not use_tqdm),
                 UpdateArtifactsCallback(),
+                TrainingLoopCallback(),
             ]
         for callback in self.callbacks:
             callback.initialize()
