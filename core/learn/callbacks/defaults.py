8fead9401ad37ad1fcbf7b2f4be8e70e9eae5e03
carefree0910 <syameimaru.saki@gmail.com>
2024-09-16 16:13:06 +0800

    🚚Moved `LogMetricsMsgCallback` -> `loggers`

--- core/learn/callbacks/defaults.py
+++ core/learn/callbacks/defaults.py
@@ -1,6 +1,5 @@
 import re
 import math
-import time
 import torch
 
 from torch import Tensor
@@ -10,15 +9,12 @@ from typing import Tuple
 from typing import Optional
 
 from ..schema import ITrainer
-from ..schema import TrainerState
 from ..schema import TrainStepLoss
-from ..schema import MetricsOutputs
 from ..schema import TrainerCallback
 from ..schedulers import WarmupScheduler
 from ..modules.common import EMA
 from ...toolkit import console
 from ...toolkit.misc import shallow_copy_dict
-from ...toolkit.misc import fix_float_to_length
 from ...toolkit.types import tensor_dict_type
 
 
@@ -36,63 +32,6 @@ class UpdateArtifactsCallback(TrainerCallback):
 
             fn = PipelineSerializer.update if update else PipelineSerializer.save
             fn(trainer.pipeline, trainer.workspace, verbose=False)  # type: ignore
-
-
-@TrainerCallback.register("log_metrics_msg")
-class LogMetricsMsgCallback(TrainerCallback):
-    def __init__(self, verbose: bool = True) -> None:
-        super().__init__()
-        self.verbose = verbose
-        self.timer = time.time()
-        self.metrics_log_path: Optional[str] = None
-
-    @staticmethod
-    def _step_str(state: TrainerState) -> str:
-        total_step = state.num_step_per_epoch
-        if state.step == -1:
-            current_step = -1
-        else:
-            current_step = state.step % total_step
-            if current_step == 0:
-                current_step = total_step if state.step > 0 else 0
-        length = len(str(total_step))
-        return f"[{current_step:{length}d} / {total_step}]"
-
-    def log_lr(self, key: str, lr: float, state: TrainerState) -> None:
-        if self.metrics_log_path is None:
-            return None
-        with open(self.metrics_log_path, "a") as f:
-            f.write(f" lr : {fix_float_to_length(lr, 12)} |\n")
-        self.metrics_log_path = None
-
-    def log_metrics_msg(
-        self,
-        metrics_outputs: MetricsOutputs,
-        metrics_log_path: str,
-        state: TrainerState,
-    ) -> None:
-        final_score = metrics_outputs.final_score
-        metric_values = metrics_outputs.metric_values
-        core = " | ".join(
-            [
-                f"{k} : {fix_float_to_length(metric_values[k], 8)}"
-                for k in sorted(metric_values)
-            ]
-        )
-        step_str = self._step_str(state)
-        timer_str = f"[{time.time() - self.timer:.3f}s]"
-        msg = (
-            f"| epoch {state.epoch:^4d} {step_str} {timer_str} | {core} | "
-            f"score : {fix_float_to_length(final_score, 8)} |"
-        )
-        if self.verbose:
-            console.log(msg)
-        with open(metrics_log_path, "a") as f:
-            if self.metrics_log_path is not None:
-                msg = f"\n{msg}"
-            f.write(msg)
-        self.timer = time.time()
-        self.metrics_log_path = metrics_log_path
 
 
 @TrainerCallback.register("training_loop")
@@ -234,7 +173,6 @@ class TrainingLoopCallback(TrainerCallback):
 
 
 __all__ = [
-    "LogMetricsMsgCallback",
     "UpdateArtifactsCallback",
     "TrainingLoopCallback",
 ]
