83ca828c7b70296a511f063fe1d58714c196969d
carefree0910 <syameimaru.saki@gmail.com>
2025-04-21 21:25:16 +0800

    ✨Implemented `StreamMSE`

--- core/learn/metrics.py
+++ core/learn/metrics.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 from .schema import IMetric
 from .schema import DataLoader
+from .schema import IStreamMetric
 from .constants import LABEL_KEY
 from .constants import PREDICTIONS_KEY
 from ..toolkit.array import corr
@@ -83,9 +84,38 @@ class Correlation(IMetric):
         return corr(predictions, labels, get_diagonal=True).mean().item()
 
 
+@IMetric.register("stream_mse")
+class StreamMSE(IStreamMetric):
+    error: float
+    num: int
+
+    @property
+    def is_positive(self) -> bool:
+        return False
+
+    def reset(self) -> None:
+        self.error = 0.0
+        self.num = 0
+
+    def update(
+        self,
+        np_batch: np_dict_type,
+        np_outputs: np_dict_type,
+        loader: Optional[DataLoader] = None,
+    ) -> None:
+        predictions = np_outputs[PREDICTIONS_KEY]
+        labels = np_batch[LABEL_KEY]
+        self.error += np.sum(np.square(labels - predictions)).item()
+        self.num += labels.size
+
+    def finalize(self) -> float:
+        return self.error / self.num if self.num > 0 else 0.0
+
+
 __all__ = [
     "MAE",
     "MSE",
     "Accuracy",
     "Correlation",
+    "StreamMSE",
 ]
